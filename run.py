7182644e41f98fd17cc91e25a53966c2d3c3861d
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2020-01-05 20:22:38 +0900

    Use f-strings and rename property to field

--- run.py
+++ run.py
@@ -3,31 +3,31 @@ import json
 import sys
 import os
 
-with open("./build-context.json") as build_context_file:
+with open('./build-context.json') as build_context_file:
   build_context = json.load(build_context_file)
 
-properties = ["username", "stage_id", "instance_id", "extension"]
+fields = ['username', 'stage_id', 'instance_id', 'extension']
 
-for property in properties:
-  if property not in build_context:
-    print("Check Property")
-    sys.exit("Check Property '{}' in Build Context".format(property))
+for field in fields:
+  if field not in build_context:
+    print('Check Property')
+    sys.exit(f"Check Property '{field}' not in Build Context")
 
-stage_id = build_context["stage_id"]
-instance_id = build_context["instance_id"]
-extension = build_context["extension"]
+stage_id = build_context['stage_id']
+instance_id = build_context['instance_id']
+extension = build_context['extension']
 
-code_path = "{}-{}.{}".format(stage_id, instance_id, extension)
+code_path = f'{stage_id}-{instance_id}.{extension}'
 
 if not os.path.exists(code_path):
-  sys.exit("Check file '{}'".format(code_path))
+  sys.exit(f"Check file '{code_path}'")
 
-os.system("g++ -o ./{} {}".format(instance_id, code_path))
+os.system(f'g++ -o ./{instance_id} {code_path}')
 
-process = subprocess.Popen(["./{}".format(instance_id)],
+process = subprocess.Popen([f'./{instance_id}'],
   stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 stdout, stderr = process.communicate()
 
 stdout = stdout.decode('utf8')
 
-print ("[*] Result : {}".format(stdout))
+print (f'[*] Result : {stdout}')
